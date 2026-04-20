2c477ced40666452a858a2592dc20d3683b2604c
HeetaeCho <cht3205@gmail.com>
2026-04-20 15:23:30 +0900

    add glomap dependency

---
+++ experiments/agent/2nd_config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ "$1" = "glomap" ]; then
+    sudo apt install -y libeigen3-dev libceres-dev libboost-all-dev libomp-20-dev libgl-dev libglu1-mesa-dev freeglut3-dev libcgal-dev libmetis-dev libfreeimage-dev
+fi
